012ded6e5115d88f85c5436371a26a2e85f2f8f4
Pham Duc Thien <S3994124@rmit.edu.vn>
2026-09-11 12:30:51 +0700

    Update config.py
    update config.py to run elastic beanstalk

--- webapp/config.py
+++ webapp/config.py
@@ -5,30 +5,28 @@ load_dotenv()
 
 
 class Config:
-    SECRET_KEY = os.getenv(
-        "SECRET_KEY",
-        "development-secret"
-    )
+    SECRET_KEY = os.getenv("SECRET_KEY","development-secret")
 
-    AWS_REGION = os.getenv(
-        "AWS_REGION",
-        "us-east-1"
-    )
+    AWS_REGION = os.getenv("AWS_REGION","us-east-1")
 
-    ANALYTICS_API_URL = ( "https://q4bibcud7b.execute-api.us-east-1.amazonaws.com" "/analytics/run")
+    ANALYTICS_API_URL = os.getenv("ANALYTICS_API_URL","https://q4bibcud7b.execute-api.us-east-1.amazonaws.com/analytics/run")
 
     S3_BUCKET = os.getenv("S3_BUCKET")
 
     RIOT_API_KEY = os.getenv("RIOT_API_KEY")
 
     USERS_TABLE = "Users"
+
     GAMES_TABLE = "Games"
+
     PLAYER_GAMES_TABLE = "PlayerGames"
 
     GROUPS_TABLE = "Groups"
+
     GROUP_MEMBERS_TABLE = "GroupMembers"
 
     INVITATIONS_TABLE = "Invitations"
 
     SESSIONS_TABLE = "Sessions"
+    
     SESSION_MEMBERS_TABLE = "SessionMembers"
